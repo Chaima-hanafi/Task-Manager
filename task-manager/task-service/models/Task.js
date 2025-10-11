87c098c353237ff206d6864948288cf3ab2fff22
ayoub <ayoub7math@gmail.com>
2025-10-11 11:45:47 +0100

    do some changes

---
+++ task-manager/task-service/models/Task.js
@@ -0,0 +1,11 @@
+const tasks = [];
+class Task {
+    constructor(id, title, descriptin, status, deadline){
+        this.id = id,
+        this.title = title, 
+        this.description = description,
+        this.status = status || 'pending',
+        this.deadline =deadline
+    };
+}
+module.exports = { Task, tasks };
