1a62f8e1e9c778d3c6ab25c7039572e165ef6ff6
ayoub <ayoub7math@gmail.com>
2025-10-11 11:47:42 +0100

    i did some changes

--- task-manager/task-service/index.js
+++ task-manager/task-service/index.js
@@ -1,1 +1,10 @@
-import express 'express';
+import express from 'express';
+import taskRoutes from './routes/TaskRoutes.js';
+
+const app = express();
+const port = 3002;
+
+app.use(express.json);
+
+app.use('/tasks',taskRoutes);
+app.listen(port, () =>{ console.log("task service running on port "+ port)});
