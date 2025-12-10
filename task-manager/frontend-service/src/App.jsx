c51b3a1eb0dd7a445a3a719645201508538c6a83
ayoub <ayoub7math@gmail.com>
2025-12-11 00:33:19 +0100

    change the front code

--- task-manager/frontend-service/src/App.jsx
+++ task-manager/frontend-service/src/App.jsx
@@ -4,11 +4,27 @@ import { useState } from 'react';
 import './App.css'
 
 function App() {
+  async function fetchTasks() {
+    try {
+      const response = await fetch('http://localhost:4001/task');
+      
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      
+      const data = await response.json();
+      console.log(data);
+      return data;
+    } catch (error) {
+      console.error('Error fetching users:', error);
+    }
+  }
+
   return (
     <div>
-      <p>This project is presented by Ayoub and Chaima</p>
+      <p>This project is presented by Ayoub and Chaima </p>
     </div>
   )
 }
 
-export default App
+export default App;
