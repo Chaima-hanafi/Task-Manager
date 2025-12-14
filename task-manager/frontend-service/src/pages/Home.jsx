f194f6ea2dd4aaf8fdaf294a00e49f88c8793fc9
ayoub <ayoub7math@gmail.com>
2025-12-14 12:07:19 +0100

    add a server to handle the frontend and ensure connexion between front and backends services

--- task-manager/frontend-service/src/pages/Home.jsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>Task Manager — Microservices</h1>
-      <p>Welcome to the React frontend.</p>
-    </div>
-  );
-}
--- task-manager/frontend-service/src/services/notificationService.js
+++
@@ -1,6 +0,0 @@
-const API = import.meta.env.VITE_NOTIFICATION_API;
-
-export async function getNotifications() {
-  const res = await fetch(`${API}/notify`);
-  return res.json();
-}
