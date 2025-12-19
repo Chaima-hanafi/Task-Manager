f5624bdf7b6a61f5f280eb20f29bc672be29c1f3
EL HANAFI <150943279+Chaima-hanafi@users.noreply.github.com.>
2025-12-19 01:01:03 +0100

    front end problem fixed

--- bashScripts/frontend-service.sh
+++ bashScripts/frontend-service.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-# Update package list
-sudo apt update -y
-
-# Install curl and git
-sudo apt install -y curl git
+# Update package list curl and git
+sudo apt update -y && sudo apt install -y curl git
 
 # Setup Node.js 22
 curl -fsSL https://deb.nodesource.com/setup_22.x | sudo -E bash -
 sudo apt install -y nodejs
-
-# Go to project directory
-cd /home/vagrant/Task-Manager/task-manager/frontend-service
-
 # Install frontend dependencies
 npm install
+# Clone the project (specific branch)
+git clone -b chaima_branch https://github.com/Chaima-hanafi/Task-Manager.git
+# Go to project directory
+cd Task-Manager/task-manager
+# Keep only frontend service
+rm -rf notification-service user-service task-service
+cd frontend-service
 
 # Start frontend service accessible from Vagrant host
 npm run dev -- --host 0.0.0.0
